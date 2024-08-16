3e1378dfcc0a0d2aadc8a0bf6ba2a65c590fe90f
allanvbcrew <allan.sebastian.perso@gmail.com>
2024-08-16 14:13:02 +0100

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
   let currentDate = new Date();
 
   // Target future date/24 hour time/Timezone
-  let targetDate = moment.tz("2024-07-19 12:00", "Europe/London");
+  let targetDate = moment.tz("2025-07-19 12:00", "Europe/London");
 
   // Calculate the difference in seconds between the future and current date
   let diff = targetDate / 1000 - currentDate.getTime() / 1000;
